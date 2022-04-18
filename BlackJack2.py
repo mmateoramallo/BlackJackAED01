58acdfa616f1005b917b4435daa86b622d55039d
Mateo <mateo.j.ramallo@gmail.com>
2022-04-18 20:43:18 -0300

    Nueva Version BlackJACK

--- BlackJack2.py
+++ BlackJack2.py
@@ -27,12 +27,16 @@ print('*'*25, 'Generando Primera Tirada', '*'*25)
 
 carta_jugador = random.choice(cartas)
 
-if carta_jugador == 'J' or carta_jugador == 'K' or carta_jugador == 'Q':
-    carta_jugador = 10
-elif carta_jugador == 'AS':
-    carta_jugador = 11
-
 palo_primera_carta_jugador = random.choice(palos)
+
+if carta_jugador == 'J' or carta_jugador == 'K' or carta_jugador == 'Q':
+    carta_jugador_letra = carta_jugador
+    carta_jugador = 10
+    print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_primera_carta_jugador)
+elif carta_jugador == 'AS':
+    carta_jugador_letra = carta_jugador
+    carta_jugador = 11
+    print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_primera_carta_jugador)
 
 print('La carta de', player, 'es el numero', carta_jugador, 'cuyo palo es', palo_primera_carta_jugador)
 
@@ -40,12 +44,16 @@ print('La carta de', player, 'es el numero', carta_jugador, 'cuyo palo es', palo
 
 carta_croupier = random.choice(cartas)
 
-if carta_croupier == 'J' or carta_croupier == 'K' or carta_croupier == 'Q':
-    carta_croupier = 10
-elif carta_croupier == 'AS':
-    carta_croupier = 11
-
 palo_primera_carta_croupier = random.choice(palos)
+
+if carta_croupier == 'J' or carta_croupier == 'K' or carta_croupier == 'Q':
+    carta_croupier_letra = carta_croupier
+    carta_croupier = 10
+    print('Carta Croupier: ', carta_croupier_letra, 'cuyo palo es', palo_primera_carta_croupier)
+elif carta_croupier == 'AS':
+    carta_croupier_letra = carta_croupier
+    carta_croupier = 11
+    print('Carta Croupier:', carta_croupier_letra, 'de', palo_primera_carta_croupier)
 
 print('Carta Croupier: ', carta_croupier, 'palo' , palo_primera_carta_croupier)
 
@@ -57,7 +65,7 @@ print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos
 
 puntuacion_croupier = carta_croupier
 
-print('-'*25,'El puntaje total del croupieres de:', puntuacion_croupier, 'puntos','-'*25)
+print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos','-'*25)
 
 
 #----------------------------------------------------#
@@ -67,12 +75,17 @@ print('*'*25, 'Generando Segunda Tirada', '*'*25)
 
 segunda_carta_jugador = random.choice(cartas)
 
-if segunda_carta_jugador == 'J' or segunda_carta_jugador == 'K' or segunda_carta_jugador == 'Q':
-    segunda_carta_jugador = 10
-elif segunda_carta_jugador == 'AS':
-    segunda_carta_jugador = 11
-
 palo_segunda_carta_jugador = random.choice(palos)
+
+if segunda_carta_jugador == 'J' or segunda_carta_jugador == 'K' or segunda_carta_jugador == 'Q':
+    carta_jugador_letra = segunda_carta_jugador
+    segunda_carta_jugador = 10
+    print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_segunda_carta_jugador)
+elif segunda_carta_jugador == 'AS':
+    carta_jugador_letra = segunda_carta_jugador
+    segunda_carta_jugador = 11
+    print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_segunda_carta_jugador)
+
 
 print('La carta de', player, 'es el numero', segunda_carta_jugador, 'cuyo palo es', palo_segunda_carta_jugador)
 
@@ -80,12 +93,18 @@ print('La carta de', player, 'es el numero', segunda_carta_jugador, 'cuyo palo e
 
 segunda_carta_croupier = random.choice(cartas)
 
-if segunda_carta_croupier == 'J' or segunda_carta_croupier == 'K' or segunda_carta_croupier == 'Q':
-    segunda_carta_croupier = 10
-elif segunda_carta_croupier == 'AS':
-    segunda_carta_croupier = 11
-
 palo_segunda_carta_croupier = random.choice(palos)
+
+if segunda_carta_croupier == 'J' or segunda_carta_croupier == 'K' or segunda_carta_croupier == 'Q':
+    carta_croupier_letra = segunda_carta_croupier
+    segunda_carta_croupier = 10
+    print('Carta Croupier: ', carta_croupier_letra, 'cuyo palo es', palo_segunda_carta_croupier)
+elif segunda_carta_croupier == 'AS':
+    carta_croupier_letra = segunda_carta_croupier
+    segunda_carta_croupier = 11
+    print('Carta Croupier:', carta_croupier_letra, 'de', palo_segunda_carta_croupier)
+
+
 
 print('Segunda Carta Croupier: ',segunda_carta_croupier, 'palo', palo_segunda_carta_croupier)
 
@@ -95,36 +114,50 @@ puntuacion_player = carta_jugador + segunda_carta_jugador
 print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos','-'*25)
 
 puntuacion_croupier = carta_croupier + segunda_carta_croupier
-print('-'*25,'El puntaje total del croupieres de:', puntuacion_croupier, 'puntos','-'*25)
+print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos','-'*25)
+
+#--------------------------> Tercera Carta, Validacion
+
+print('*'*25, 'Generando Tercera Tirada', '*'*25)
+
+#Obtencion De Tercera Carta Para el jugador
+
+if puntuacion_player < 21:
+
+    tercera_carta_jugador = random.choice(cartas)
 
 
-#Validamos el puntaje del croupier para saber si debe obtener una nueva carta, "el crupier está obligado a plantarse si suma 17 o más".
+if puntuacion_croupier <= 16:
+    if puntuacion_croupier >= 17:
+        print('Fin de la Mano Para el Croupier')
 
-if puntuacion_croupier <= 17:
-    print('No puede Recibir Mas Cartas')
-    print('-' * 25, 'El puntaje total del croupieres de:', puntuacion_croupier, 'puntos', '-' * 25)
-else:
-    #En caso de que no se cumpla esa condicion el croupier esta habilitado a levantar una tercera carta
     tercera_carta_croupier = random.choice(cartas)
 
+    palo_tercera_carta_croupier = random.choice(palos)
+
     if tercera_carta_croupier == 'J' or tercera_carta_croupier == 'K' or tercera_carta_croupier == 'Q':
+        carta_croupier_letra = tercera_carta_croupier
         tercera_carta_croupier = 10
-    elif tercera_carta_croupier == 'AS' and puntuacion_croupier < 21:
+        print('Carta Croupier: ', carta_croupier_letra, ' cuyo palo es' , palo_tercera_carta_croupier)
+    elif tercera_carta_croupier == 'AS':
+        carta_croupier_letra = tercera_carta_croupier
         tercera_carta_croupier = 11
-#Mostramos el puntaje del croupier
-puntuacion_croupier = carta_croupier + segunda_carta_croupier + tercera_carta_croupier
-print('-'*25,'El puntaje total del croupieres de:', puntuacion_croupier, 'puntos','-'*25)
+        print('Carta Croupier: ', carta_croupier_letra, 'de', palo_tercera_carta_croupier)
 
-#El jugador no está obligado a seguir esas dos reglas, y puede plantarse cuando quiera o seguir pidiendo cartas mientras no sume 21, Validamos el puntaje del jugador para saber si puede obtener una tercera carta
+    print('Tercera Carta Croupier:', tercera_carta_croupier, 'palo', palo_tercera_carta_croupier)
 
-if puntuacion_player > 21:
-    print('No puede Recibir Mas Cartas')
-    print('-' * 25, 'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos', '-' * 25)
+
+
+
 
 #Obtener puntuacion total
+
+print()
+print()
+print()
 
 puntuacion_player = carta_jugador + segunda_carta_jugador
 print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos','-'*25)
 
 puntuacion_croupier = carta_croupier + segunda_carta_croupier
-print('-'*25,'El puntaje total del croupieres de:', puntuacion_croupier, 'puntos','-'*25)
+print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos','-'*25)
