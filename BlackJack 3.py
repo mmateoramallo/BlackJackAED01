b27d7c05a6c2eee11f6179498cc824bfc3b1ff58
Mateo <mateo.j.ramallo@gmail.com>
2022-04-18 23:32:28 -0300

    Nueva Version BlackJACK

--- BlackJack 3.py
+++ BlackJack 3.py
@@ -124,44 +124,74 @@ tercera_carta_jugador = random.choice(cartas)
 palo_tercera_carta_jugador = random.choice(palos)
 
 if puntuacion_player <= 16:
-    # Al negar la proposicion estamos diciendo que el bloque de codigo se cumplira siempre y cuando la puntuacion del jugador no supere los 17 puntos
-    if not(puntuacion_player >= 17):
+    if (puntuacion_player >= 17):
+        print('No puede Levantar Cartas debido a su puntaje')
+        puntuacion_player = carta_jugador + segunda_carta_jugador
+        print('-' * 25, 'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos', '-' * 25)
+    else:
+
         if tercera_carta_jugador == 'J' or tercera_carta_jugador == 'K' or tercera_carta_jugador == 'Q':
             carta_jugador_letra = tercera_carta_jugador
             tercera_carta_jugador = 10
             print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
         elif tercera_carta_jugador == 'AS':
-            #Si la puntuacion del jugador no supera
-            if not(puntuacion_player > 21):
+            # Si la puntuacion del jugador no supera los 21
+            if not (puntuacion_player > 21):
+                carta_jugador_letra = tercera_carta_jugador
+                tercera_carta_jugador = 11
+                print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
+            else:
+                carta_jugador_letra = tercera_carta_jugador
+                tercera_carta_jugador = 1
+                print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
+
+        # Puntajes Finales
+
+        puntuacion_player = carta_jugador + segunda_carta_jugador + tercera_carta_jugador
+        print('-' * 25, 'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos', '-' * 25)
 
 
+#Tercera Carta del Croupier
 
+tercera_carta_croupier = random.choice(cartas)
 
+palo_tercera_carta_croupier = random.choice(palos)
 
-if tercera_carta_jugador == 'J' or tercera_carta_jugador == 'K' or tercera_carta_jugador == 'Q':
-    carta_jugador_letra = tercera_carta_jugador
-    tercera_carta_jugador = 10
-    print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
-elif tercera_carta_jugador == 'AS':
-    if puntuacion_player > 21:
-        carta_jugador_letra = segunda_carta_jugador
-        tercera_carta_jugador = 1
-        print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
+if puntuacion_croupier <= 16:
+    if (puntuacion_croupier >= 17):
+        print('No puede Levantar Cartas debido a su puntaje')
+        puntuacion_croupier = carta_croupier + segunda_carta_croupier
+        print('-' * 25, 'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos', '-' * 25)
     else:
-        carta_jugador_letra = segunda_carta_jugador
-        tercera_carta_jugador = 11
+        if tercera_carta_croupier == 'J' or tercera_carta_croupier == 'K' or tercera_carta_croupier == 'Q':
+            carta_croupier_letra = tercera_carta_croupier
+            tercera_carta_croupier = 10
+            print('Carta Croupier: ', carta_croupier_letra, ' cuyo palo es', palo_tercera_carta_croupier)
+        elif tercera_carta_croupier == 'AS':
+            if not (puntuacion_croupier > 21):
+                carta_croupier_letra = tercera_carta_croupier
+                tercera_carta_croupier = 11
+                print('Carta Croupier: ', carta_croupier_letra, 'de', palo_tercera_carta_croupier)
+            else:
+                carta_croupier_letra = tercera_carta_croupier
+                tercera_carta_croupier = 1
+                print('Carta Croupier: ', carta_croupier_letra, 'de', palo_tercera_carta_croupier)
+
+        # Puntajes Finales
+
+        puntuacion_croupier = carta_croupier + segunda_carta_croupier + tercera_carta_croupier
+        print('-' * 25, 'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos', '-' * 25)
 
 
 
 
 
+'''
+#Puntajes Finales
 
-
-
-#Puntajes Hasta Ahora
-
-puntuacion_player = carta_jugador + segunda_carta_jugador
+puntuacion_player = carta_jugador + segunda_carta_jugador + tercera_carta_jugador
 print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos','-'*25)
 
-puntuacion_croupier = carta_croupier + segunda_carta_croupier
+puntuacion_croupier = carta_croupier + segunda_carta_croupier + tercera_carta_croupier
 print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos','-'*25)
+'''
