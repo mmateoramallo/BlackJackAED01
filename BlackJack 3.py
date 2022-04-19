62a0436d4e31f866712a6b2934fe6422d5c29c35
Mateo <mateo.j.ramallo@gmail.com>
2022-04-19 08:17:18 -0300

    Nueva Version BlackJACK

--- BlackJack 3.py
+++ BlackJack 3.py
@@ -1,6 +1,10 @@
 #Importando el modulo random
 import random
 
+#Importamos PySimpleGUI, para una sorpresa
+import PySimpleGUI as psg
+
+
 print('♠'*25, 'BlackJack', '♠'*25)
 
 #Asignacion nombres, para declarar el ganador
@@ -20,6 +24,7 @@ cartas = (2,3,4,5,6,7,8,9,10,'J', 'Q', 'K', 'AS')
 
 palos = ('Picas', 'Trebol', 'Corazones', 'Diamantes')
 
+cantidad_de_figuras = 0
 
 #Generar Aleatoriamente los palos y valores
 
@@ -30,12 +35,17 @@ carta_jugador = random.choice(cartas)
 palo_primera_carta_jugador = random.choice(palos)
 
 if carta_jugador == 'J' or carta_jugador == 'K' or carta_jugador == 'Q':
+    cantidad_de_figuras += 1
+
     carta_jugador_letra = carta_jugador
     carta_jugador = 10
+
     print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_primera_carta_jugador)
 elif carta_jugador == 'AS':
+
     carta_jugador_letra = carta_jugador
     carta_jugador = 11
+
     print('La carta de', player, 'es el', carta_jugador_letra, 'cuyo palo es', palo_primera_carta_jugador)
 
 print('La carta de', player, 'es el numero', carta_jugador, 'cuyo palo es', palo_primera_carta_jugador)
@@ -47,8 +57,11 @@ carta_croupier = random.choice(cartas)
 palo_primera_carta_croupier = random.choice(palos)
 
 if carta_croupier == 'J' or carta_croupier == 'K' or carta_croupier == 'Q':
+    cantidad_de_figuras += 1
+
     carta_croupier_letra = carta_croupier
     carta_croupier = 10
+
     print('Carta Croupier: ', carta_croupier_letra, 'cuyo palo es', palo_primera_carta_croupier)
 elif carta_croupier == 'AS':
     carta_croupier_letra = carta_croupier
@@ -59,6 +72,8 @@ print('Carta Croupier: ', carta_croupier, 'palo' , palo_primera_carta_croupier)
 
 #Puntaje De Ambos Jugadores
 
+print()
+
 puntuacion_player = carta_jugador
 
 print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos','-'*25)
@@ -68,8 +83,13 @@ puntuacion_croupier = carta_croupier
 print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos','-'*25)
 
 
+
 #----------------------------------------------------#
 #Segunda Mano De Cartas
+
+print()
+print()
+print()
 
 print('♢'*25, 'Generando Segunda Tirada', '♢'*25)
 
@@ -78,8 +98,11 @@ segunda_carta_jugador = random.choice(cartas)
 palo_segunda_carta_jugador = random.choice(palos)
 
 if segunda_carta_jugador == 'J' or segunda_carta_jugador == 'K' or segunda_carta_jugador == 'Q':
+    cantidad_de_figuras += 1
+
     carta_jugador_letra = segunda_carta_jugador
     segunda_carta_jugador = 10
+
     print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_segunda_carta_jugador)
 elif segunda_carta_jugador == 'AS':
     carta_jugador_letra = segunda_carta_jugador
@@ -96,8 +119,11 @@ segunda_carta_croupier = random.choice(cartas)
 palo_segunda_carta_croupier = random.choice(palos)
 
 if segunda_carta_croupier == 'J' or segunda_carta_croupier == 'K' or segunda_carta_croupier == 'Q':
+    cantidad_de_figuras += 1
+
     carta_croupier_letra = segunda_carta_croupier
     segunda_carta_croupier = 10
+
     print('Carta Croupier: ', carta_croupier_letra, 'cuyo palo es', palo_segunda_carta_croupier)
 elif segunda_carta_croupier == 'AS':
     carta_croupier_letra = segunda_carta_croupier
@@ -106,7 +132,11 @@ elif segunda_carta_croupier == 'AS':
 
 print('Carta Croupier:',  segunda_carta_croupier, 'palo', palo_segunda_carta_croupier)
 
+
 #-->Obtencion de puntajes
+
+print()
+
 puntuacion_player = carta_jugador + segunda_carta_jugador
 print('-'*25,'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos','-'*25)
 
@@ -116,7 +146,13 @@ print('-'*25,'El puntaje total del croupier es de:', puntuacion_croupier, 'punto
 
 #----------------------------------------------------#
 
+print()
+print()
+print()
+
 print('♢'*25, 'Generando Tercera Tirada', '♢'*25)
+
+print()
 
 #Tercera Carta del jugador
 
@@ -132,8 +168,11 @@ if puntuacion_player <= 16:
     else:
 
         if tercera_carta_jugador == 'J' or tercera_carta_jugador == 'K' or tercera_carta_jugador == 'Q':
+            cantidad_de_figuras += 1
+
             carta_jugador_letra = tercera_carta_jugador
             tercera_carta_jugador = 10
+
             print('La carta de', player, 'es la', carta_jugador_letra, 'cuyo palo es', palo_tercera_carta_jugador)
         elif tercera_carta_jugador == 'AS':
             # Si la puntuacion del jugador no supera los 21
@@ -165,6 +204,9 @@ if puntuacion_croupier <= 16:
         print('-' * 25, 'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos', '-' * 25)
     else:
         if tercera_carta_croupier == 'J' or tercera_carta_croupier == 'K' or tercera_carta_croupier == 'Q':
+
+            cantidad_de_figuras += 1
+
             carta_croupier_letra = tercera_carta_croupier
             tercera_carta_croupier = 10
             print('Carta Croupier: ', carta_croupier_letra, ' cuyo palo es', palo_tercera_carta_croupier)
@@ -187,20 +229,22 @@ if puntuacion_croupier <= 16:
 
 #Puntajes Finales
 
-print('-'*21, 'Resultados', '-'*21)
+print('♠'*21, 'Resultados', '♠'*21)
+
+print()
+print()
+
 print('-' * 25, 'El puntaje total de', player, 'es de:', puntuacion_player, 'puntos', '-' * 25)
 print('-' * 25, 'El puntaje total del croupier es de:', puntuacion_croupier, 'puntos', '-' * 25)
 
 #Quien Obtuvo el mayor puntaje
 if puntuacion_player > puntuacion_croupier:
-    print('-' * 21, 'Felicidades!!!!', '-' * 21)
     print('-' * 11, player,'tuvo mas puntos!!!!', '-' * 1)
 else:
-    print('-' * 21, 'Intenta Nuevamente!!!!', '-' * 21)
     print('-' * 11, 'El Crupier tuvo mas puntos!!!!', '-' * 11)
 
-#Determinar Ganador
 
+#Determinar Ganador
 
 #Para saber quien estuvo mas cerca resto las puntuaciones y luego comparo los resultados de la resta
 
@@ -222,6 +266,23 @@ elif distancia_player > distancia_croupier:
     ganador = 'Croupier'
     print('-' * 11,'El ganador es', ganador, '-' * 11)
 elif distancia_player == distancia_croupier:
-    print('-'*21,'Felicidades Ambos Ganaron!!!!', '-'*21)
-    print('-'*21,'Empataron Ambos Jugadores', '-'*21)
+    print('-'*21,'Felicidades Ambos Empataron!!!!', '-'*21)
+
+#################
+#----> Determinar si el palo de la primera carta es el mismo para ambos
+
+if palo_primera_carta_jugador == palo_primera_carta_croupier:
+    print('-' * 20 + '>', 'Las primeras cartas tienen el mismo palo')
+    mismos_palos = True
+else:
+    mismos_palos = False
+
+#Si además de coincidir en el palo es el mismo número (tomando en consideración que las cartas se pueden repetir) o figura mostrar un mensaje adicional.
+if mismos_palos and (carta_jugador == carta_croupier):
+    psg.popup("Las primeras cartas COINCIDEN en PALO Y NUMERO!")
+
+if cantidad_de_figuras >= 1:
+    print('-' * 20 + '>', 'Salieron', cantidad_de_figuras , 'figuras')
+
+
 
